854df3dbb64c9abc936c6be721da165e772cafee
anton <anton@LAPTOP-B1GN23L2>
2026-02-23 16:19:14 +0200

    add cors permissions

--- StoryChain.Api/Program.cs
+++ StoryChain.Api/Program.cs
@@ -19,10 +19,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
             .AllowAnyHeader()
             .AllowAnyMethod()
-            .AllowCredentials();
+            .AllowCredentials()
+            .SetIsOriginAllowed(origin => true);
     });
 });
 
