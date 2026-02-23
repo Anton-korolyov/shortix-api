cd9a81fb5fd5d231c296651ef657293dd0e358d4
anton <anton@LAPTOP-B1GN23L2>
2026-02-23 13:37:15 +0200

    Set postgres connection string for shortix

--- StoryChain.Api/obj/Debug/net8.0/StoryChain.Api.AssemblyInfo.cs
+++ StoryChain.Api/obj/Debug/net8.0/StoryChain.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoryChain.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd526fda197f6b549283a4e59528a0fe4d9d7688")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoryChain.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoryChain.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
