0a3e46ba49ca33b05702061897edb295c862bfba
anton <anton@LAPTOP-B1GN23L2>
2026-02-23 16:57:48 +0200

    add cors permissions

--- StoryChain.Api/obj/Debug/net8.0/StoryChain.Api.AssemblyInfo.cs
+++ StoryChain.Api/obj/Debug/net8.0/StoryChain.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoryChain.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+416b1443649ca66fbc5c40564f095100f2b1b9f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075f6cafb29a9572eac5f43056206cd55e8612a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoryChain.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoryChain.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
