753b7cdb144c31435284a97024af93f4773e5b0a
anton <anton@LAPTOP-B1GN23L2>
2026-04-12 09:41:09 +0300

    add explore controller new video

--- StoryChain.Api/obj/Debug/net8.0/StoryChain.Api.AssemblyInfo.cs
+++ StoryChain.Api/obj/Debug/net8.0/StoryChain.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoryChain.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+518ba9a5977b947142ea6c82533d5afd34593d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce74b04a928c18c55116d2f9bfc189d5e35500f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoryChain.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoryChain.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
